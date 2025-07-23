6dd86f650c70f48a2567d9e3239f02d4f20c4303
⚡️🌩️war goddess 300⚡️ <154751143+warGoddess300@users.noreply.github.com>
2025-07-23 15:56:12 -0400

    Add files via upload

---
+++ Bot.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/bash
+
+#telling the computer which shebang i'm using
+
+#warGoddess300
+
+#training coding bash
+
+
+echo -e "Hello $USER,\nwhich command do you want me to execute for you: ?"
+
+array=("useping" "useIRC" "usenetcat")
+
+for i in {0..2}; do
+	echo -e "Select a command: $i. ${array[i]}"
+
+done
+
+read -p "Enter a number according selecting a command output: " array_num
+
+
+case $array_num in
+	0)
+		read -p "Enter a domain name or IP address: " ip_domain
+		ping $ip_domain
+		;;
+
+	1)
+		read -p "Enter an IRC server or IRC IP address: " irc_server
+		#irc command
+
+		echo "irc unavailable"
+		;;
+
+	2)
+		read -p "Enter a command to use netcat: " netcat_use
+		#netcat command after
+		echo "netcat command unavailable"
+		;;
+
+	*)
+		echo "Invalid command"
+		;;
+esac
+
